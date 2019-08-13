80fd5342bceea84e7e5b5a40c60fc6246e520fd3
Chiuaru Bogdan <bchiuaru94@gmail.com>
2019-08-13 18:56:44 +0300

    Wrote the code for the last lesson.

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -1,15 +1,20 @@
 package org.fasttrackit;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import org.fasttrackit.persistance.ToDoItemRepository;
 
-    
+import java.io.IOException;
+import java.sql.SQLException;
+
+
+public class App
+{
+    public static void main( String[] args ) throws SQLException, IOException, ClassNotFoundException {
+        ToDoItemRepository toDoItemRepository = new ToDoItemRepository();
+        //toDoItemRepository.createToDoItem("Learn Java",LocalDateTime.now().plusMonths(6));
+        //toDoItemRepository.deleteToDoItem(2);
+        System.out.println(toDoItemRepository.getToDoItems());
+
+        toDoItemRepository.updateToDoItem(1,true);
+        System.out.println(toDoItemRepository.getToDoItems());
+    }
 }
